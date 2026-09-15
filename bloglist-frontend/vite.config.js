135fe2a1d49808da5143d4ef6be990a8e2b847c1
johnmalnyuon <wmal44884@gmail.com>
2026-09-15 13:51:10 +0300

    the frontend is fix

--- bloglist-frontend/vite.config.js
+++ bloglist-frontend/vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["fullstack-own-pipeline.onrender.com"],
     proxy: {
-      '/api': {
-        target: 'http://localhost:3003',
-        changeOrigin: true
-      }
-    }
+      "/api": {
+        target: "http://localhost:3003",
+        changeOrigin: true,
+      },
+    },
   },
 
   test: {
     globals: true,
-    environment: 'jsdom',
-  }
-})
+    environment: "jsdom",
+  },
+  preview: {
+    allowedHosts: ["fullstack-own-pipeline.onrender.com"],
+  },
+});
